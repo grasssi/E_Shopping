500c78826263051914a96003c14bc8d5fa2ec6b2
GroSSo <grassi.amine@gmail.com>
2022-04-15 11:54:09 +0100

    Adding Content to the Recipes Components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipies: Recipe[] = [
+    new Recipe('A test Reipe','This is simply a tes','https://scontent.ftun18-1.fna.fbcdn.net/v/t31.18172-8/893702_438656382884762_495850160_o.jpg?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=4NruTaeBQB4AX9ncHfE&_nc_ht=scontent.ftun18-1.fna&oh=00_AT8zS3ipszWWJCLQu-R7lcyHvx4mSix6HptHgF8jVg3AWg&oe=627F3353')
+  ]
   constructor() { }
 
   ngOnInit(): void {
